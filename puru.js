8196735173ef2a32e977d9c237ecfabe0b3082fa
PurvakJ <137681540+PurvakJ@users.noreply.github.com>
2023-12-15 07:22:39 +0530

    Add files via upload

---
+++ puru.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Set a 3-second delay for hiding the loader
+    setTimeout(function () {
+        var loaderElement = document.getElementById('loading');
+        if (loaderElement) {
+            loaderElement.style.display = 'none';
+        }
+    }, 0);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const animatedElements = document.querySelectorAll('.animated');
+
+  function checkScroll() {
+    animatedElements.forEach((element) => {
+      let scrollPosition = window.scrollY + window.innerHeight;
+      let elementPosition = element.offsetTop + element.offsetHeight;
+
+      if (scrollPosition > elementPosition) {
+        element.classList.add('animate');
+      }
+    });
+  }
+
+  // Initial check in case the elements are already visible on page load
+  checkScroll();
+
+  // Listen for scroll events
+  window.addEventListener('scroll', checkScroll);
+});
